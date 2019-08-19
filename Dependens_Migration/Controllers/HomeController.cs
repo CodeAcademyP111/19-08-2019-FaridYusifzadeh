c0a4a98643abedc41ee0c8e8553841b11736b261
FaridYusifzadeh <fariddsx2015@gmail.com>
2019-08-19 17:44:28 +0400

    Home Work

---
+++ Dependens_Migration/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dependens_Migration.DAL;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dependens_Migration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context;
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+    }
+}
---
+++ Dependens_Migration/DAL/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Dependens_Migration.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dependens_Migration.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+    }
+}
